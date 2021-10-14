c1df904086e0a9e98c906978ee2564470f800cd9
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2021-10-14 12:12:25 -0500

    adding the section footer and information

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 const iconClose = document.querySelector(".iconClose");
-console.log(iconClose);
 
 hamburger.addEventListener("click", (e) => {
   menu.style.display = "block";
